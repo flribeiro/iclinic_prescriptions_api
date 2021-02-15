87d9ef2316d13eed6a429076617876e4703d7db1
Fabrício Lombardi Ribeiro <fabricio.ribeiro@luizalabs.com>
2021-02-15 10:39:30 -0300

    feat: add error catalog

---
+++ prescriptions/errors/error_catalog.py
@@ -0,0 +1,26 @@
+errors = {
+    1: {
+        'message': 'malformed requeset',
+        'code': '01'
+    },
+    2: {
+        'message': 'physician not found',
+        'code': '02'
+    },
+    3: {
+        'message': 'patient not found',
+        'code': '03'
+    },
+    4: {
+        'message': 'metrics service not available',
+        'code': '04'
+    },
+    5: {
+        'message': 'physicians service not available',
+        'code': '05'
+    },
+    6: {
+        'message': 'patients service not available',
+        'code': '06'
+    },
+}
