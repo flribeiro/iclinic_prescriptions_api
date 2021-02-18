cc0c68483e7f58e35a3de364fab663d8dd9b3ab6
Fabrício Lombardi Ribeiro <fabricio.ribeiro@luizalabs.com>
2021-02-18 02:32:54 -0300

    fix: adjust cache config to get connection from env file

--- iclinic/settings.py
+++ iclinic/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = '/static/'
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'LOCATION': env('REDIS_LOCATION'),
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient'
         }
